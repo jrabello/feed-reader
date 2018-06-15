cf11a43322a6a9359fc39927c0910a1f27a7ee60
jrabelo <joilson.rabello@gmail.com>
2018-06-14 22:45:05 -0300

    Adds fixes from review

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -14,22 +14,12 @@ $(function() {
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+        
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        
         it('has URLs', function() {
             allFeeds.forEach(feed => {
                 expect(feed.url).toBeDefined(); 
@@ -37,10 +27,6 @@ $(function() {
             });
         });
         
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
         it('has Names', function() {
             allFeeds.forEach(feed => {
                 expect(feed.name).toBeDefined(); 
@@ -48,16 +34,11 @@ $(function() {
             });
         });
     });
-
-    /* TODO: Write a new test suite named "The menu" */
+    
     describe('The menu', () =>  {
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        
         it('is hidden', function() {
-            expect(document.querySelector(`body`).className).toBe(`menu-hidden`); 
+            expect($(`body`).hasClass(`menu-hidden`)).toBe(true); 
         });
 
         /* TODO: Write a test that ensures the menu changes
@@ -68,11 +49,11 @@ $(function() {
          it('is hidden when clicked', function() {
             //expect displayed when clicked
             document.querySelector(`.menu-icon-link`).click()
-            expect(document.querySelector(`body`).className).not.toBe(`menu-hidden`); 
+            expect($(`body`).hasClass(`menu-hidden`)).toBe(false); 
 
             //expect hidden when clicked again
             document.querySelector(`.menu-icon-link`).click()
-            expect(document.querySelector(`body`).className).toBe(`menu-hidden`); 
+            expect($(`body`).hasClass(`menu-hidden`)).toBe(true); 
         });
     });
 
@@ -85,13 +66,11 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
         beforeEach(function(done) {
-            loadFeed(0, function() {
-                done();
-            })
+            loadFeed(0, done)
         });
 
         it('loadFeed completes', function(done) {
-            expect(document.getElementsByClassName(`feed`)[0].children.length > 0).toBe(true); 
+            expect($('.feed').text().length > 0).toBe(true); 
             done();
         });
     });
@@ -102,17 +81,20 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-        let oldFeedLength = 0;
+        let oldFeedText;
+        let newFeedText;
         beforeEach(function(done) {
-            oldFeedLength = document.getElementsByClassName(`feed`)[0].children[0].innerText;
-            loadFeed(1, function() {
-                done();
-            })
+            loadFeed(0, () => {
+                oldFeedText = $('.feed').text();
+                loadFeed(1, () => {
+                    newFeedText = $('.feed').text();
+                    done()
+                });
+            });
         });
 
         it('loadFeed changes', function(done) {
-            const newFeedLength = document.getElementsByClassName(`feed`)[0].children[0].innerText;
-            expect(newFeedLength !== oldFeedLength).toBe(true); 
+            expect(oldFeedText && newFeedText && oldFeedText !== newFeedText).toBe(true); 
             done();
         });
     });
